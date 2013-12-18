2e1e553f07f1f9d1ed4a2a96433f55aa54e5e049
vvo <vincent.voyer@gmail.com>
2013-12-18 19:38:26 +0100

    use selenium 2.39

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 // see https://code.google.com/p/selenium/downloads/list for latest
-var version = '2.38.0';
+var version = '2.39.0';
 
 module.exports = {
   selenium: {
