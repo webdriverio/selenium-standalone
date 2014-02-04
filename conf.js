16a65052e4e856fd7f7aa60901d61f510f090909
vvo <vincent.voyer@gmail.com>
2014-02-04 10:33:06 +0100

    upgrade chromedriver 2.9

--- conf.js
+++ conf.js
@@ -11,6 +11,6 @@ module.exports = {
   chromeDr: {
     path: path.join(__dirname, '.selenium', version, 'chromedriver'),
     // see http://chromedriver.storage.googleapis.com/index.html
-    v: '2.8'
+    v: '2.9'
   }
 };
