5355c0604adbe5849346df5c07e71c74612432ec
Brandon Collins <brandon@carlypso.com>
2015-01-03 16:15:50 -0800

    update config settings
    conf.js assumed hub url port of 4444
    if -port 222 is passed as selenium server argument
    the server will start but then get killed after 60s timeout
    added simple argument parser specifically looking for
    a port passed as selenium standalone argument

--- conf.js
+++ conf.js
@@ -2,12 +2,19 @@ var path = require('path');
 
 // see http://selenium-release.storage.googleapis.com/index.html for latest
 var version = process.env.SELENIUM_VERSION || '2.44.0';
+var argvStr = process.argv.slice(2).join(' ');
+
+// http://www.seleniumhq.org/docs/07_selenium_grid.jsp
+// if you want to run selenium on a different port
+// matches assuming selenium standalone argument style e.g. java -jar selenium-server-standalone.jar -port 2222
+var port = argvStr.match(/-port ([\d]+)/i);
+port = port ? port[1] : 4444;
 
 module.exports = {
   selenium: {
     path: path.join(__dirname, '.selenium', version, 'server.jar'),
     v: version,
-    hub: 'http://localhost:4444/wd/hub/status'
+    hub: 'http://localhost:' + port + '/wd/hub/status'
   },
   chromeDr: {
     path: path.join(__dirname, '.selenium', version, 'chromedriver'),
