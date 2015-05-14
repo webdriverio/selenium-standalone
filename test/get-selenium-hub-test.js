2c36492f0bc0fa969975203ad52043ed8f2643e8
Ben Plessinger <ben@plessinger.us>
2015-05-13 23:40:54 -0400

    update to better handle server config options and not error out
    make requested edits

--- test/get-selenium-hub-test.js
+++ test/get-selenium-hub-test.js
@@ -1,24 +1,28 @@
-var assert = require("assert")
-var getSeleniumHub = require('../lib/get-selenium-hub');
+var assert = require("assert");
+var getSeleniumStatusUrl = require('../lib/get-selenium-status-url');
+var nodeUriPath = '/wd/hub/status';
 
-describe('getSeleniumHub', function () {
-  var data = [{args: [], expectedHostPort: 'localhost:4444'},
-              {args: ['-port', '5555'], expectedHostPort: 'localhost:5555'},
-              {args: ['-hub', 'http://foo/wd/register'], expectedHostPort: 'foo:4444'},
-              {args: ['-hub', 'http://foo:6666/wd/register'], expectedHostPort: 'foo:6666'},
-              {args: ['-hub', 'http://foo/wd/register', '-port', '7777'], expectedHostPort: 'foo:7777'},
-              {args: ['-hub', 'http://foo:6666/wd/register', '-port', '7777'], expectedHostPort: 'foo:7777'}];
+describe('getSeleniumStatusUrl', function () {
+  var data = [{args: [], expectedUrl: 'localhost:4444' + nodeUriPath},
+              {args: ['-port', '5555'], expectedUrl: 'localhost:5555' + nodeUriPath},
+              {args: ['-hub', 'http://foo/wd/register'], expectedUrl: 'foo:4444' + nodeUriPath},
+              {args: ['-role', 'hub'], expectedUrl: 'localhost:4444/api/grid/hub/'},
+              {args: ['-role', 'node'], expectedUrl: 'localhost:5555' + nodeUriPath},
+              {args: ['-role', 'node', '-hub', 'http://foo:6666/wd/register'], expectedUrl: 'foo:6666' + nodeUriPath},
+              {args: ['-hub', 'http://foo:6666/wd/register'], expectedUrl: 'foo:6666' + nodeUriPath},
+              {args: ['-hub', 'http://foo/wd/register', '-port', '7777'], expectedUrl: 'foo:7777' + nodeUriPath},
+              {args: ['-hub', 'http://foo:6666/wd/register', '-port', '7777'], expectedUrl: 'foo:7777' + nodeUriPath}];
 
   var testWithData = function (dataItem) {
     return function () {
-      var actual = getSeleniumHub(dataItem.args);
-      var expected = 'http://' + dataItem.expectedHostPort + '/wd/hub/status';
+      var actual = getSeleniumStatusUrl(dataItem.args);
+      var expected = 'http://' + dataItem.expectedUrl;
 
       assert.equal(actual, expected);
     };
   };
 
   data.forEach(function (dataItem) {
-    it("getSeleniumHub with seleniumArgs: " + dataItem.args.join(" "), testWithData(dataItem));
+    it('getSeleniumStatusUrl with seleniumArgs: ' + dataItem.args.join(' '), testWithData(dataItem));
   });
 });
