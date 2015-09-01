a08ef8871df131855b5867649b10a340fafd0076
vvo <vincent.voyer@algolia.com>
2015-09-01 12:36:49 +0200

    fix: add empty stderr stdout listeners only if not already here
    fixes #91

--- test/programmatic.js
+++ test/programmatic.js
@@ -12,4 +12,20 @@ describe('programmatic use', function () {
       done();
     });
   });
+
+  it('can listen to stderr', function(done) {
+    this.timeout(60000);
+    var selenium = require('../');
+    selenium.start(function(err, cp) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      cp.stderr.once('data', function() {
+        cp.kill();
+        done();
+      });
+    });
+  });
 });
