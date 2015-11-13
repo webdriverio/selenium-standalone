779486779ad8cab5b9d4c166e592421aa1c7fd83
Stephen Ash <sash@adobe.com>
2015-11-13 13:54:39 -0800

    Update test cases to expect correct URLs

--- test/get-selenium-hub-test.js
+++ test/get-selenium-hub-test.js
@@ -1,17 +1,25 @@
 var assert = require("assert");
 var getSeleniumStatusUrl = require('../lib/get-selenium-status-url');
-var nodeUriPath = '/wd/hub/status';
+var nodeStatusAPIPath = '/wd/hub/status';
+var hubStatusAPIPath = '/grid/api/hub';
 
 describe('getSeleniumStatusUrl', function () {
-  var data = [{args: [], expectedUrl: 'localhost:4444' + nodeUriPath},
-              {args: ['-port', '5555'], expectedUrl: 'localhost:5555' + nodeUriPath},
-              {args: ['-hub', 'http://foo/wd/register'], expectedUrl: 'foo:4444' + nodeUriPath},
-              {args: ['-role', 'hub'], expectedUrl: 'localhost:4444/api/grid/hub/'},
-              {args: ['-role', 'node'], expectedUrl: 'localhost:5555' + nodeUriPath},
-              {args: ['-role', 'node', '-hub', 'http://foo:6666/wd/register'], expectedUrl: 'foo:6666' + nodeUriPath},
-              {args: ['-hub', 'http://foo:6666/wd/register'], expectedUrl: 'foo:6666' + nodeUriPath},
-              {args: ['-hub', 'http://foo/wd/register', '-port', '7777'], expectedUrl: 'foo:7777' + nodeUriPath},
-              {args: ['-hub', 'http://foo:6666/wd/register', '-port', '7777'], expectedUrl: 'foo:7777' + nodeUriPath}];
+  var data = [
+              // If started with both modes active, check the status of the `node`
+              {args: [], expectedUrl: 'localhost:4444' + nodeStatusAPIPath},
+              {args: ['-port', '5555'], expectedUrl: 'localhost:5555' + nodeStatusAPIPath},
+              {args: ['-hub', 'http://foo/wd/register'], expectedUrl: 'foo:4444' + nodeStatusAPIPath},
+              {args: ['-hub', 'http://foo:6666/wd/register'], expectedUrl: 'foo:6666' + nodeStatusAPIPath},
+              {args: ['-hub', 'http://foo/wd/register', '-port', '7777'], expectedUrl: 'foo:7777' + nodeStatusAPIPath},
+              {args: ['-hub', 'http://foo:6666/wd/register', '-port', '7777'], expectedUrl: 'foo:7777' + nodeStatusAPIPath},
+
+              // If started with only `hub` mode, only check status of itself
+              {args: ['-role', 'hub'], expectedUrl: 'localhost:4444' + hubStatusAPIPath},
+
+              // If started with only `node` mode, check status of remote hub instance
+              {args: ['-role', 'node'], expectedUrl: 'localhost:5555' + hubStatusAPIPath},
+              {args: ['-role', 'node', '-hub', 'http://foo:6666/wd/register'], expectedUrl: 'foo:6666' + hubStatusAPIPath}
+            ];
 
   var testWithData = function (dataItem) {
     return function () {
