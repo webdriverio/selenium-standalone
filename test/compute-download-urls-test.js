e81b05639091b089360eb9b7937d48974ec9236f
Stephen Ash <stephenash@gmail.com>
2016-09-15 09:57:30 -0700

    Cleanup code comments

--- test/compute-download-urls-test.js
+++ test/compute-download-urls-test.js
@@ -2,32 +2,6 @@ var assert = require('assert');
 
 var computeDownloadUrls;
 
-/*
-default-config
-
-module.exports = {
-  baseURL: 'https://selenium-release.storage.googleapis.com',
-  version: '2.53.1',
-  drivers: {
-    chrome: {
-      version: '2.23',
-      arch: process.arch,
-      baseURL: 'https://chromedriver.storage.googleapis.com'
-    },
-    ie: {
-      version: '2.53.1',
-      arch: process.arch,
-      baseURL: 'https://selenium-release.storage.googleapis.com'
-    },
-    firefox: {
-      version: '0.10.0',
-      arch: process.arch,
-      baseURL: 'https://github.com/mozilla/geckodriver/releases/download'
-    }
-  }
-};
-*/
-
 /**
  * Tests for the `computeDownloadUrls` module.
  * 
