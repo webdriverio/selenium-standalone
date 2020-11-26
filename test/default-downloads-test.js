86b27150cff702518535dd30197bd69584d35e87
Mike Grybyk <mykola.grybyk@servicehouse.nl>
2020-11-26 14:55:18 +0100

    reduce packages size

--- test/default-downloads-test.js
+++ test/default-downloads-test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const merge = require('lodash').merge;
+const merge = require('lodash.merge');
 const got = require('got');
 
 const defaultConfig = require('../lib/default-config');
