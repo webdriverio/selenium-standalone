3182e237a578199b77460f858ccb208fee0e1ee8
Eric Anderson <afflicted.nimbly928@anonaddy.me>
2022-05-02 11:45:13 -0400

    Partially revert 2da8f60 (checking only top-level folder)
    When a file within a folder is updated the folder does not change it's
    timestamp. Only if a new file/folder is added/removed will the
    timestamp change.
    
    The commit message on 2da8f60 doesn't make clear if the developer
    thought the top-level folder would update the timestamp but that is
    my assumption. It's also not clear what failure was happening with the
    test but my assumption is what it was the etag issue I'm work to
    correct.
    
    This commit updates the test so it does the folder walk it was doing
    previously and verifies that ALL files have the same timestamp. Since
    the previous code to walk the tree was made a while back I did make
    some style changes to match with current project conventions.

--- test/no-reinstall.js
+++ test/no-reinstall.js
@@ -8,13 +8,35 @@ describe('when files are installed', () => {
     const path = require('path');
     const targetDir = path.join(__dirname, '..', '.selenium');
     const selenium = require('..');
-    const origDirModifTime = fs.statSync(targetDir).mtime.getTime();
+
+    // Recursively find files in the given directory
+    function walk(dirname, files = []) {
+      fs.readdirSync(dirname).forEach((name) => {
+        const filepath = path.join(dirname, name);
+        if (fs.statSync(filepath).isDirectory()) {
+          walk(filepath, files);
+        } else {
+          files.push(filepath);
+        }
+      });
+      return files;
+    }
+    const installedFiles = walk(targetDir);
+
+    // Get last modified time of files that should already be installed in
+    // the .selenium directory.
+    const mtimes = installedFiles.reduce((acc, filepath) => {
+      acc[filepath] = fs.statSync(filepath).mtime.getTime();
+      return acc;
+    }, {});
 
     // Compare last modified time of files after running the installation
     // again. It shouldn't download any files, otherwise it fails.
     await selenium.install();
-    const currentDirModifTime = fs.statSync(targetDir).mtime.getTime();
-    const isModified = currentDirModifTime > origDirModifTime;
+
+    const isModified = !installedFiles.every((filepath) => {
+      return mtimes[filepath] === fs.statSync(filepath).mtime.getTime();
+    });
 
     assert.strictEqual(isModified, false, 'It should not have reinstalled files');
   });
