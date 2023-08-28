3728383d505ec9fc0ed1af2f9df955d21b1d6f96
Siarhei Kliushnikau <77188735+udarrr@users.noreply.github.com>
2023-08-28 18:44:21 +0200

    killing by default (#841)
    * killing by default
    
    * test fixes
    
    * test fix

--- docs/API.md
+++ docs/API.md
@@ -111,7 +111,7 @@ By default all drivers are loaded, you only control and change the versions or a
 
 `opts.requestOpts` can be any valid [`got` options object](https://www.npmjs.com/package/got#proxies). You can use this for example to set a timeout.
 
-`opts.processKiller` set for that truthy value, for killing selenium server port.
+`opts.processKiller` set to falsy value, for preventing killing selenium server port.
 
 returns `Promise<ChildProcess>`
 
--- test/process-killer-test.js
+++ test/process-killer-test.js
@@ -10,7 +10,7 @@ let opts = {
   drivers: defaultConfig.drivers,
 };
 
-describe('check usual a start is going to be without killing processes', () => {
+describe('check usual a start by default', () => {
   before(async () => {
     await processKiller({}, ':4444');
   });
@@ -20,10 +20,6 @@ describe('check usual a start is going to be without killing processes', () => {
   });
 
   it('check usual a start', async () => {
-    opts = merge(opts, {
-      processKiller: true,
-    });
-
     try {
       await start(opts);
     } catch (err) {
@@ -32,7 +28,7 @@ describe('check usual a start is going to be without killing processes', () => {
   });
 });
 
-describe('check killing before starting with truthy processKiller property', () => {
+describe('check killing before starting by default', () => {
   before(async () => {
     await processKiller({}, ':4444');
   });
@@ -50,12 +46,12 @@ describe('check killing before starting with truthy processKiller property', () 
       await start(opts);
       await start(opts);
     } catch (err) {
-      assert(!err.message.includes('Port 4444 is already in use'));
+      assert(false);
     }
   });
 });
 
-describe('check killing before starting without processKiller property', () => {
+describe('check killing before when started twice', () => {
   before(async () => {
     await processKiller({}, ':4444');
   });
@@ -64,18 +60,13 @@ describe('check killing before starting without processKiller property', () => {
     await processKiller({}, ':4444');
   });
 
-  it('start selenium server twice without processKiller', async () => {
-    let testErr;
-
-    delete opts.processKiller;
-
+  it('start selenium server twice', async () => {
     try {
       await start(opts);
       await start(opts);
     } catch (err) {
-      testErr = err;
+      assert(false);
     }
-    assert(!testErr || ('message' in testErr && testErr.message.includes('Port 4444 is already in use')));
   });
 });
 
@@ -101,6 +92,6 @@ describe('check killing before starting with falsy processKiller property', () =
     } catch (err) {
       testErr = err;
     }
-    assert(!testErr || ('message' in testErr && testErr.message.includes('Port 4444 is already in use')));
+    assert(testErr && testErr.message.includes('Port 4444 is already in use'));
   });
 });
